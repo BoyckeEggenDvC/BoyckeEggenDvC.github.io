ea411e2eed527f11de0be52b56417e694fc815f2
BoyckeEggenDvC <beggen@davinci.nl>
2025-05-07 20:22:56 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,13 +1,13 @@
 function setGreen()
 {
-    var oi = document.body
+    var oi = document.body;
     oi.classList.remove("red");
     oi.classList.add("green");
 
 }
 function setRed()
 {
-    var oi = document.body
+    var oi = document.body;
     oi.classList.remove("green");
     oi.classList.add("red");
 }
