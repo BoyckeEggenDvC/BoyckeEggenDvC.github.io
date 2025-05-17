aeafa0cfcebb48d91a9842c8314b476bc071c0f4
BoyckeEggenDvC <beggen@davinci.nl>
2025-05-17 23:08:28 +0200

    redone

---
+++ OnEens/functions.js
@@ -0,0 +1,13 @@
+function setGreen()
+{
+    var oi = document.body;
+    oi.classList.remove("red");
+    oi.classList.add("green");
+
+}
+function setRed()
+{
+    var oi = document.body;
+    oi.classList.remove("green");
+    oi.classList.add("red");
+}
---
+++ main.js
@@ -0,0 +1,11 @@
+var i = 0
+
+function MoveRight(){
+    i = i+1;
+    if(i == 1){
+    scrollTo(100,200)
+    }else
+    {
+    scrollTo(3000,200)
+    }
+}
